655541f5bc4f808e8348343e1824e7b86eeb1551
Kanhaiya Mohan <kanhaiyamohan@Kanhaiyas-MacBook-Air.local>
2022-12-29 00:47:21 +0530

    Postman Debug

--- src/main/java/com/food/delivery/app/ws/model/response/RequestOperationName.java
+++
@@ -1,5 +0,0 @@
-package com.food.delivery.app.ws.model.response;
-
-public enum RequestOperationName {
-	DELETE
-}
--- src/main/java/com/food/delivery/app/ws/model/response/RequestOperationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.food.delivery.app.ws.model.response;
-
-public enum RequestOperationStatus {
-	ERROR, SUCCESS
-}
